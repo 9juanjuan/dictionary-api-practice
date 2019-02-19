f88d6137956ae99d35b195f9dd44a1a2b2610c93
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-02-18 19:39:44 -0500

    starts game of thrones practice

---
+++ game-of-thrones.py
@@ -0,0 +1,57 @@
+from characters import characters
+print(len(characters))
+jon_snow = {"url":"https://anapioficeandfire.com/api/characters/583","name":"Jon Snow","gender":"Male","culture":"Northmen","born":"In 283 AC","died":"","titles":["Lord Commander of the Night's Watch"],"aliases":["Lord Snow","Ned Stark's Bastard","The Snow of Winterfell","The Crow-Come-Over","The 998th Lord Commander of the Night's Watch","The Bastard of Winterfell","The Black Bastard of the Wall","Lord Crow"],"father":"","mother":"","spouse":"","allegiances":["https://anapioficeandfire.com/api/houses/362"],"books":["https://anapioficeandfire.com/api/books/5"],"povBooks":["https://anapioficeandfire.com/api/books/1","https://anapioficeandfire.com/api/books/2","https://anapioficeandfire.com/api/books/3","https://anapioficeandfire.com/api/books/8"],"tvSeries":["Season 1","Season 2","Season 3","Season 4","Season 5","Season 6"],"playedBy":["Kit Harington"]}
+
+counter = 0
+# for character in characters:
+#     if (character["name"][0] == "A") and (character["name"] != ""):
+#             counter +=1
+# print (counter)
+# for character in characters:
+#     if len(character["died"]) >= 1:
+#         counter +=1 
+#         # print (character["name"])
+# print(counter)
+
+# for character in characters:
+#     for titles in character["titles"]:
+#         print (max(len(titles)))
+        
+# for character in characters:
+#     if character["culture"] == "Valyrian":
+#         counter +=1
+
+# print (counter)
+
+for character in characters:
+    if character["name"]== "Hot Pie":
+        print (character["playedBy"])
+
+for character in characters:
+    for series in character["tvSeries"]:
+        if len(series) == 0:
+            counter += 1
+            #1927 characters not in TV series
+# print (counter)
+
+
+
+
+
+
+# for character in characters:
+#     for character["titles"] in character:
+#         for title in character["titles"]:
+        
+    
+# # print out the key names individually
+# for k in jon_snow:
+#     print(k)
+
+# # print out just the values
+# for k in jon_snow:
+#     print(jon_snow[k])
+
+# # print both the key and the value
+# for k in jon_snow:
+#     print("%s: %s" % (k, jon_snow[k]))
